47a23e5f0e2cdc1516f6c04c57272d98d24c3ebf
Mahedi Hasan Niloy <niloy.oct@gmail.com>
2023-09-26 11:23:23 +0600

    instade of using routing based api, convert this into controller based api.

---
+++ RestFulAPI/Controllers/ShirtController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RestFulAPI.Controllers
+{
+    [ApiController]
+    public class ShirtController : ControllerBase
+    {
+        [HttpGet]
+        [Route("/shirts")]
+        public string GetShirts()
+        {
+            return "all the shirts";
+        }
+
+        [HttpGet]
+        [Route("/shirts/{id}")]
+        public string GetShirtsById(int id)
+        {
+            return $"Reading  shirts with ID : {id}";
+        }
+
+        [HttpPut]
+        [Route("/shirts/{id}")]
+        public string UpdateShirts(int id)
+        {
+            return $"Update  shirts with ID : {id}";
+        }
+
+
+        [HttpDelete]
+        [Route("/shirts/{id}")]
+        public string DeleteShirtsById(int id)
+        {
+            return $"Delete  shirts with ID : {id}";
+        }
+    }
+}
--- RestFulAPI/Program.cs
+++ RestFulAPI/Program.cs
@@ -1,4 +1,5 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
 
 // Add services to the container.
 
@@ -7,36 +8,40 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
+app.MapControllers();
+//app.UseRouting();
+//app.UseAuthentication();
+//app.UseAuthorization();
 
 //Routing
 
 // let say we build a api for an shirt selling website
 
-app.MapGet("/shirts", () =>
-{
-    return "Reading all the shirts";
-});
+//app.MapGet("/shirts", () =>
+//{
+//    return "Reading all the shirts";
+//});
 
-app.MapGet("/shirts/{id}", (int id) =>
-{
-    return $"Reading  shirts with ID : {id}";
-});
+//app.MapGet("/shirts/{id}", (int id) =>
+//{
+//    return $"Reading  shirts with ID : {id}";
+//});
 
 
-app.MapPost("/shirts", () =>
-{
-    return "Create new shirt";
-});
+//app.MapPost("/shirts", () =>
+//{
+//    return "Create new shirt";
+//});
 
-app.MapPut("/shirts/{id}", (int id) =>
-{
-    return $"Update shirt with ID : {id}";
-});
+//app.MapPut("/shirts/{id}", (int id) =>
+//{
+//    return $"Update shirt with ID : {id}";
+//});
 
-app.MapDelete("/shirts/{id}", (int id) =>
-{
-    return $"Delete shirt with ID : {id}";
-});
+//app.MapDelete("/shirts/{id}", (int id) =>
+//{
+//    return $"Delete shirt with ID : {id}";
+//});
 
 app.Run();
 
